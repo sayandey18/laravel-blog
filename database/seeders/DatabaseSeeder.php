974425df524b683d900ef9976f6f414d56ddb8d7
Sayan <mr.sayandey18@outlook.com>
2024-06-09 21:06:56 +0530

    Update the seeders with dummy cat and tags

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -4,19 +4,22 @@ namespace Database\Seeders;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use App\Models\Category;
+use App\Models\Tag;
 
 class DatabaseSeeder extends Seeder
 {
     /**
      * Seed the application's database.
      */
-    public function run(): void
+    public function run()
     {
-        // \App\Models\User::factory(10)->create();
+        Category::create(['name' => 'Technology', 'active' => true]);
+        Category::create(['name' => 'Health', 'active' => true]);
+        Category::create(['name' => 'Lifestyle', 'active' => false]);
 
-        // \App\Models\User::factory()->create([
-        //     'name' => 'Test User',
-        //     'email' => 'test@example.com',
-        // ]);
+        Tag::create(['name' => 'PHP', 'active' => true]);
+        Tag::create(['name' => 'Laravel', 'active' => true]);
+        Tag::create(['name' => 'JavaScript', 'active' => false]);
     }
 }
