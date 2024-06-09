0c7093393722b1638e225d42455708f29a4975a9
Sayan <mr.sayandey18@outlook.com>
2024-06-09 21:23:54 +0530

    Create database Models

---
+++ app/Models/Post.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Str;
+
+class Post extends Model
+{
+    use HasFactory;
+
+    protected $fillable = ['title', 'slug', 'body', 'category_id'];
+
+    protected static function boot() {
+        parent::boot();
+
+        static::creating(function ($post) {
+            $post->slug = Str::slug($post->title);
+        });
+
+        static::updating(function ($post) {
+            $post->slug = Str::slug($post->title);
+        });
+    }
+
+    public function category() {
+        return $this->belongsTo(Category::class);
+    }
+
+    public function tags() {
+        return $this->belongsToMany(Tag::class);
+    }
+}
