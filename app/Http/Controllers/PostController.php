f29723df5945fb621d29a14b1e1de233d022b89b
Sayan <mr.sayandey18@outlook.com>
2024-06-09 21:18:11 +0530

    Create controller for handle Post

---
+++ app/Http/Controllers/PostController.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class PostController extends Controller
+{
+    public function index() {
+        
+    }
+
+    public function destroy(string $id) {
+
+    }
+}
