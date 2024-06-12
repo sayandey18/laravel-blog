ce801410eef1a6c727842821036393b340488df0
Sayan <mr.sayandey18@outlook.com>
2024-06-12 15:34:30 +0530

    Install CKEditor for posts

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,6 +1,20 @@
 import './bootstrap';
 import './custom-script';
 import Alpine from 'alpinejs';
+import ClassicEditor from '@ckeditor/ckeditor5-build-classic';
 
 window.Alpine = Alpine;
 Alpine.start();
+
+const ckEditor = document.querySelector('#editor');
+if (ckEditor) {
+    ClassicEditor
+    .create(ckEditor)
+    .then(editor => {
+        const form = document.querySelector('form');
+        form.addEventListener('submit', () => {
+            editor.updateSourceElement();
+        });
+    })
+    .catch( error => { console.error( error ); } );
+}
