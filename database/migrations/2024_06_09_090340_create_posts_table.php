3ca5338634576d90bb6c9093a77cbc893ac2779c
Sayan <mr.sayandey18@outlook.com>
2024-06-12 15:30:57 +0530

    Replace the foreign key with user_id

--- database/migrations/2024_06_09_090340_create_posts_table.php
+++ database/migrations/2024_06_09_090340_create_posts_table.php
@@ -13,10 +13,10 @@ return new class extends Migration
     {
         Schema::create('posts', function (Blueprint $table) {
             $table->id();
+            $table->foreignId('user_id')->constrained()->onDelete('cascade');
             $table->string('title');
             $table->string('slug')->unique();
             $table->text('body');
-            $table->foreignId('category_id')->constrained()->onDelete('cascade');
             $table->timestamps();
         });
     }
